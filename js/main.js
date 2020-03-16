b311ec9b63396ca1671b68b5d853079d1c081882
sivertillya <volk.sivert@gmail.com>
2020-03-16 17:04:52 +0200

    Сomplete!

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    siv_link.onclick = siv_link;
+    gus_link.onclick = gus_link;
+    //id.on<событие> = nameFuction;
+    function siv_link() {
+        alert("Сайт Іллі");
+    };
+    function gus_link() {
+        alert("Сайт Едіка");
+    };
+});
---
+++ site/siv/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    elem.onclick = link;
+    //id.on<событие> = nameFuction;
+    function link() {
+        alert("Help me!");
+    };
+});
