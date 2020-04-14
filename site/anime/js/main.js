ef8e0ac8646c44a9686a044d135aa16b219344c7
sivertillya <volk.sivert@gmail.com>
2020-04-14 21:09:41 +0300

    Add anime site

---
+++ site/anime/js/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    // Sign in
+    login = prompt('Login:', '');
+    password = prompt('Password:', '');
+    login_siv = "siv";
+    password_siv = "qwe";
+    login_gus = "gus";
+    password_gus = "zxc";
+    login_test = "test";
+    password_test = "123";
+    if (login == login_siv && password == password_siv || login == login_gus && password == password_gus || login == login_test && password == password_test) {
+        alert("YES");
+    }
+    else if (login == login_siv || login == login_gus || login == login_test) {
+        alert('Пароль неверный!'), top.location.href="https://sivertillya.github.io/"
+    }
+    else { alert('Логин не найден!'), top.location.href="https://sivertillya.github.io/" }
+
+
+
+    // alert("Сайт Іллі");
+    // elem.onclick = link;
+    // //id.on<событие> = nameFuction;
+    // function link() {
+    //     alert("Help me!");
+    // };
+
+
+
+
+    $( "#viss" ).mousedown(function() {
+        pass.setAttribute("type", "text");
+        viss.setAttribute("class", "fa fa-eye");
+    });
+    $( "#viss" ).mouseup(function() {
+        pass.setAttribute("type", "password");
+        viss.setAttribute("class", "fa fa-eye-slash");
+    });
+
+});
