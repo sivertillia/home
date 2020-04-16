a453ba86b9608f72d975b5fbf6b8494501dd5acc
sivertillya <volk.sivert@gmail.com>
2020-04-16 15:44:42 +0300

    add site video

--- site/anime/js/main.js
+++ site/anime/js/main.js
@@ -11,20 +11,4 @@ $(document).ready(function() {
 
 
 
-
-    function check_for_no_anime(){
-        var show_some=false;
-    }
-    $(".all_anime_global").each(function(indx,element){
-        if($(element).css("display")!="none"){show_some=true;return false;}
-    });
-    if($("#dle-content").outerHeight(){
-        $(".anime_choose_wall").outerHeight()+$(".mailBlock_h").outerHeight();
-        $("#dle-content").css({"min-height":($(".anime_choose_wall").outerHeight()+$(".mailBlock_h").outerHeight())+"px"});
-    }
-    if(show_some) {
-        if ($(".anime_choose_no_anime_top").css("display") != "none") {
-            $(".anime_choose_no_anime_top").fadeOut(fade_anime_time)
-        };
-    };
 });
