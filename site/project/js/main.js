57508e1cb44c83b425ad24afb795c57ddccac176
sivertillya <volk.sivert@gmail.com>
2020-07-04 20:04:17 +0300

    add game

---
+++ site/project/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    var downButton = document.querySelector('.download');
+    downButton.onclick = function() {
+        document.location.href = "file/game.exe";
+    };
+
+
+
+
+
+});
