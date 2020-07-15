8412f4f0d0befd1be802adc0290cdf20f9981af3
sivertillya <volk.sivert@gmail.com>
2020-07-15 20:48:53 +0300

    2.0 app

--- site/project/js/main.js
+++ site/project/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     var downButton = document.querySelector('.download');
     downButton.onclick = function() {
-        document.location.href = "file/app.apk";
+        document.location.href = "file/app2.0.apk";
     };
 
 
