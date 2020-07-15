1ef0e4a985058dbf4bde2f45fafea6d3eaed8047
sivertillya <volk.sivert@gmail.com>
2020-07-15 17:28:15 +0300

    Add apk

--- site/project/js/main.js
+++ site/project/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     var downButton = document.querySelector('.download');
     downButton.onclick = function() {
-        document.location.href = "file/game.exe";
+        document.location.href = "file/app.apk";
     };
 
 
