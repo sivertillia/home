4f093bf28192668ce1f0163b481f62561310d864
sivertillya <volk.sivert@gmail.com>
2020-08-12 22:24:18 +0300

    update

---
+++ site/oxxxymiron/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    var downButtonOne = document.querySelector('.download-one');
+	var downButtonTwo = document.querySelector('.download-two');
+	
+	
+    downButtonOne.onclick = function() {
+        document.location.href = "file/miXXXtape I (1).zip";
+    };
+    downButtonTwo.onclick = function() {
+        document.location.href = "file/miXXXtape I (2).zip";
+    };
+
+
+
+
+});
