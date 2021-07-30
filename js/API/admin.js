f38cf5acb7243cc6ba105987e4d2cf4e32dc689c
GunITy <volk.sivert@gmail.com>
2021-07-30 20:39:38 +0300

    add new view project

--- js/API/admin.js
+++ js/API/admin.js
@@ -1,23 +1,53 @@
 const token = sessionStorage.getItem('access_token');
 if (!token) location.href = 'file:///G:/project/MY-GIT/home/login/index.html';
 
+const PATH_URL = 'https://sivertillya-server.herokuapp.com/'
+const PATH_CREATE = 'create_project/'
+
 $(document).ready(() => {
+    const errorText = document.querySelector('.error_text_js');
+
     const logout = () => {
         sessionStorage.clear();
         location.reload();
     }
 
     const createProject = async () => {
-        let nameProject = $("[name=\"name\"]").val();
-        let linkProject = $("[name=\"link\"]").val();
-        let nameAdditionalLink = $("[name=\"name_additional_link\"]").val();
-        let additionalLink = $("[name=\"additional_link\"]").val();
-        let description = $("[name=\"description\"]").val();
+        console.log("asdasdasdasdsa");
+        let project_name = $("[name=\"project_name\"]").val();
+        let project_link = $("[name=\"project_link\"]").val();
+        let additional_name_link = $("[name=\"additional_name_link\"]").val();
+        let additional_link = $("[name=\"additional_link\"]").val();
         let date = $("[name=\"date\"]").val();
-        let img = $("[name=\"img\"]").val();
-        console.log(typeof(img));
+        let url_photo = $("[name=\"url_photo\"]").val();
+        let detail = $("[name=\"detail\"]").val();
 
-        var base64ImageContent = img.replace(/^data:image\/(png|jpg);base64,/, "");
+        const data = {
+            project_name: project_name,
+            project_link: project_link,
+            additional_name_link: additional_name_link,
+            additional_link: additional_link,
+            date: date,
+            url_photo: url_photo,
+            detail: detail
+        }
+
+        let response = await fetch(`${PATH_URL}${PATH_CREATE}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        let json = await response.json()
+        if (response.ok) {
+            errorText.innerHTML = null
+            console.log(json);
+        } else {
+            // viewError(error);
+            errorText.innerHTML = json['error']
+            console.log(json);
+        }
     }
 
     document.querySelector('#btnLogout').onclick = logout;
--- js/main.js
+++ js/main.js
@@ -1,4 +1,66 @@
+
+
+
+
+
 $(document).ready(function() {
+    const PATH_URL = 'https://sivertillya-server.herokuapp.com/'
+    const PATH_PROJECTS = 'projects/'
+    const block = document.getElementById('projects_js');
+
+    const block_project = ''
+
+    const viewProject = (projects) => {
+        for (let i = 0; i < projects.length; i++){
+            console.log(projects[i]['additional_name_link'] != 'null');
+            if ((projects[i]['additional_name_link'] != 'null') && (projects[i]['additional_link'] != 'null')) {
+                block.innerHTML += `<div class="project"><a class="project_img" href="${projects[i]['url_photo']}" data-fancybox data-caption="Caption #1"><img src="${projects[i]['url_photo']}" alt=""></a><a href="${projects[i]['project_link']}" target="_blank"><h2>${projects[i]['project_name']}</h2></a><a href="${projects[i]['additional_link']}" target="_blank"><h4>${projects[i]['additional_name_link']}</h4></a><p>${projects[i]['detail'].split('\n')[0]}</p><p>${projects[i]['detail'].split('\n')[1]}</p></div>`
+            } else {
+                block.innerHTML += `<div class="project"><a class="project_img" href="${projects[i]['url_photo']}" data-fancybox data-caption="Caption #1"><img src="${projects[i]['url_photo']}" alt=""></a><a href="${projects[i]['project_link']}" target="_blank"><h2>${projects[i]['project_name']}</h2></a><p>${projects[i]['detail'].split('\n')[0]}</p><p>${projects[i]['detail'].split('\n')[1]}</p></div>`
+            }
+        }
+        console.log(projects.length);
+    }
+
+    const getProject = async () => {
+        let response = await fetch(`${PATH_URL}${PATH_PROJECTS}`, {
+            method: 'GET',
+        })
+        let json = await response.json()
+        if (response.ok) {
+            viewProject(json)
+        } else {
+            console.log(json);
+        }
+    }
+
+    getProject()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     function scrollTo( target ) {
         if( target.length ) {
             $("html, body").stop().animate( { scrollTop: target.offset().top }, 1000);
